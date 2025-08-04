930fe513a95181557d69278383570ddb61be93b4
root <lapique.philippe@gmail.com>
2025-08-04 15:29:39 +0000

    Fun : Function count leaves for the tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Count the depth.
+ * binary_tree_leaves - Count the leaves.
  * @tree: Pointer to the parent node
  *
  *Return: size_t count.
@@ -9,5 +9,19 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+size_t count = 0;
 
+if (tree == NULL)
+	{
+	return (0);
+	}
+if (tree->left == NULL && tree->right == NULL)
+	{
+	count++;
+	}
+else
+	{
+	count += binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	}
+return (count);
 }
