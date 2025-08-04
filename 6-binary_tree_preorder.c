2b7af335866bbe58bf18bf0d23eceeb325d9449c
root <lapique.philippe@gmail.com>
2025-08-04 13:24:00 +0000

    Add file funcion binary_preoder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
