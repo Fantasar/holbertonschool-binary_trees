0ef8a9469c98d6b096d07d0ebe03c4c19dd90313
root <lapique.philippe@gmail.com>
2025-08-04 12:46:38 +0000

    feat : prototype exercice 4 binary_leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+
+
+}
