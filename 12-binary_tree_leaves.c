c1d6a5687f46dc9772ceb4a88464d81f494c9dad
root <lapique.philippe@gmail.com>
2025-08-04 15:04:55 +0000

    Add file 12-binary_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Count the depth.
+ * @tree: Pointer to the parent node
+ *
+ *Return: size_t count.
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+}
