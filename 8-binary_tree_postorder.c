9f729e53491748a48abaa47129df5d584522c6c9
root <lapique.philippe@gmail.com>
2025-08-04 14:19:27 +0000

    Add file 8-binary for postorder tree

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Preorder the tree node
+ * @tree: Pointer to the parent node
+ * @func: Value to put in the new node
+ *
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
