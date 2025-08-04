c8da539d84305f5170ed8ec5d227c8ef675ae7c0
root <lapique.philippe@gmail.com>
2025-08-04 14:37:01 +0000

    Add file 10 binary_three_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Preorder the tree node
+ * @tree: Pointer to the parent node
+ * @func: Value to put in the new node
+ *
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+}
