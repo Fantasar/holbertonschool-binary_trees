904b08e6b684c8458d94526a6521d417c3629a51
root <lapique.philippe@gmail.com>
2025-08-04 12:35:39 +0000

    Add file 2-binary_Right : file for new tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node
+ * binary_tree_insert_right - Creates a binary tree node at right
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  *
@@ -10,5 +10,26 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+binary_tree_t *new_node;
+if (parent == NULL)
+	{
+	return (NULL);
+	}
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+	{
+	return (NULL);
+	}
+new_node->n = value;
+new_node->parent = parent;
+new_node->right = parent->right;
+new_node->left = NULL;
 
+if (parent->right != NULL)
+	{
+	parent->right->parent = new_node;
+	}
+parent->right = new_node;
+
+return (new_node);
 }
