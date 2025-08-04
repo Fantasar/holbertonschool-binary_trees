2df06e082849152c6707a13d0ea8c9f09c709dda
root <lapique.philippe@gmail.com>
2025-08-04 14:11:33 +0000

    Add file 6-binary_tree_preorder.c for check the tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,14 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node
- * @value: Value to put in the new node
+ * binary_tree_preorder - Preorder the tree node
+ * @tree: Pointer to the parent node
+ * @func: Value to put in the new node
  *
- * Return: Pointer to the new node, or NULL on failure
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 
+if (tree == NULL || func == NULL)
+	{
+	return;
+	}
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
 }
