67a3a8ddadcc2ab0a0d54fe977b64606a9e62ef3
root <lapique.philippe@gmail.com>
2025-08-04 14:55:35 +0000

    add function for count depth tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,13 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Preorder the tree node
+ * binary_tree_depth - Count the depth.
  * @tree: Pointer to the parent node
- * @func: Value to put in the new node
  *
+ *Return: size_t count.
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+size_t count = 0;
 
+if (tree == NULL)
+	{
+	return (0);
+	}
+while (tree->parent != NULL)
+	{
+	tree = tree->parent;
+	count++;
+	}
+return (count);
 }
